15a13cc9816db63509aa2bb3cb946df507fa4c4e
xiaocheng <2034585952@qq.com>
2023-01-20 16:31:30 +0800

    初始化项目

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,5 @@
+// 像计算属性，针对依赖于state的状态
+// 这里的app user是模块，因为在index.js中是以模块的方式注册的
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,8 @@ import user from './modules/user'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  // 以模块的方式注册
+  // 以模块方式注册的值，state.子模块.属性名就能取到值了，因为每个模块开启了命名空间，所以调用其中的值要加模块名
   modules: {
     app,
     settings,
